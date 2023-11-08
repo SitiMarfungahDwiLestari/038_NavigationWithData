331e00527bc9097a54a3b8fd4cdf676e285eb19a
Siti Marfungah Dwi Lestari <siti.marfungah.ft21@mail.umy.ac.id>
2023-11-08 23:29:09 +0700

    Initial commit

---
+++ app/src/main/java/com/example/esjumbo/data/SumberData.kt
@@ -0,0 +1,12 @@
+package com.example.esjumbo.data
+import com.example.esjumbo.R
+
+object SumberData {
+    val flavor = listOf(
+        R.string.natural,
+        R.string.vanila,
+        R.string.lemon,
+        R.string.milk,
+        R.string.lyche,
+    )
+}
