6880e1930274ccb44bc5973ac0b421094396321f
Siti Marfungah Dwi Lestari <siti.marfungah.ft21@mail.umy.ac.id>
2023-11-16 09:05:58 +0700

    menambahkan nama, alamat, dan no hp agar bisa tertampil di halaman summary

--- app/src/main/java/com/example/esjumbo/HalamanDua.kt
+++ app/src/main/java/com/example/esjumbo/HalamanDua.kt
@@ -27,6 +27,9 @@ fun HalamanDua(
     modifier: Modifier = Modifier
     ){
     val items = listOf(
+        Pair(stringResource(R.string.nama), orderUiState.nama),
+        Pair(stringResource(R.string.no), orderUiState.noHP),
+        Pair(stringResource(R.string.alamat), orderUiState.alamat),
         Pair(stringResource(R.string.quantity), orderUiState.jumlah),
         Pair(stringResource(R.string.flavor), orderUiState.rasa)
     )
